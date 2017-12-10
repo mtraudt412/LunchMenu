3843f59a17c6d4829bf2ffe029f7e6800553abfa
mtraudt <mtraudt@quantifisolutions.com>
2017-12-10 14:14:18 -0500

    Initial support for MenuRequests

--- main.go
+++ main.go
@@ -1,76 +1,241 @@
-package hello
+// Package LunchMenu serves up static HTML pages and responds to REST requests
+// to enable lunch menus to be stored, updated, queried, and to enable homeroom
+// teachers to specify how many of each item are requested by their students.
+package LunchMenu
 
 import (
-  "encoding/json"
-  "github.com/gorilla/mux"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
 	"google.golang.org/appengine"
-  "google.golang.org/appengine/datastore"
-  "net/http"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/log"
 )
 
+// Menu represents the menu items (as a JSON list) for a specific date.
 type Menu struct {
-  Date string // Must be formatted as YYYYMMDD (e.g. 20170901)
-  Items string // Comma-separated list of menu items (e.g. Pizza, Grilled cheese)
+	Date  string
+	Items string
+}
+
+// MenuDto represents the menu items (as a list) for the specific date.
+type MenuDto struct {
+	Date  string
+	Items []string
+}
+
+// MenuRequest represents the count of each menu item (as a JSON map) requested by a homeroom for a specific date.
+type MenuRequest struct {
+	Date     string
+	Homeroom string
+	Choices  string
+}
+
+// MenuRequestDto represents the count of each menu item (as a map) requested by a homeroom for a specific date.
+type MenuRequestDto struct {
+	Date     string
+	Homeroom string
+	Choices  map[string]int
 }
 
 func init() {
-  r := mux.NewRouter()
-  r.HandleFunc("/menus", getMenus).Methods("GET")
-  r.HandleFunc("/menus/{date}", getMenu).Methods("GET")
-  r.HandleFunc("/menus/{date}", addMenu).Methods("POST")
-  r.PathPrefix("/scripts/").Handler(http.StripPrefix("/scripts/", http.FileServer(http.Dir("scripts"))))
-  r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-  r.Path("/").Handler(http.FileServer(http.Dir("static")))
-  http.Handle("/", r)
-}
+	r := mux.NewRouter()
 
-func getMenus(w http.ResponseWriter, r *http.Request) {
-  c := appengine.NewContext(r)
-  q := datastore.NewQuery("Menu").Order("-Date")
-  var menus []Menu
-  _, err := q.GetAll(c, &menus)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-  json.NewEncoder(w).Encode(menus)
+	// Menu CRUD operations
+	r.HandleFunc("/menus/{date}", getMenu).Methods("GET")
+	r.HandleFunc("/menus/{date}", createMenu).Methods("POST")
+	r.HandleFunc("/menus/{date}", deleteMenu).Methods("DELETE")
+
+	// MenuRequest CRUD operations
+	r.HandleFunc("/requests/{date}", getMenuRequests).Methods("GET")
+	r.HandleFunc("/requests/{date}/{homeroom}", createMenuRequest).Methods("POST")
+	r.HandleFunc("/requests/{date}/{homeroom}", deleteMenuRequest).Methods("DELETE")
+
+	// Static content
+	r.PathPrefix("/scripts/").Handler(http.StripPrefix("/scripts/", http.FileServer(http.Dir("scripts"))))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	r.Path("/").Handler(http.FileServer(http.Dir("static")))
+
+	http.Handle("/", r)
 }
 
 func getMenu(w http.ResponseWriter, r *http.Request) {
-  c := appengine.NewContext(r)
-  menu := new(Menu)
-  params := mux.Vars(r)
-  date := params["date"]
-  key := datastore.NewKey(c, "Menu", date, 0, nil)
-  err := datastore.Get(c, key, menu)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-  err = json.NewEncoder(w).Encode(menu)
-  if err != nil {
-    panic(err)
-  }
+	c := appengine.NewContext(r)
+
+	// Get date from URL
+	params := mux.Vars(r)
+	date := params["date"]
+
+	// Query
+	menu := new(Menu)
+	key := datastore.NewKey(c, "Menu", date, 0, nil)
+	err := datastore.Get(c, key, menu)
+	if err == datastore.ErrNoSuchEntity {
+		log.Infof(c, "No menu found for [%s]", date)
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Convert to DTO
+	dto := new(MenuDto)
+	dto.Date = date
+	dto.Items = make([]string, 0)
+	err = json.Unmarshal([]byte(menu.Items), &dto.Items)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Send as JSON
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(dto)
+	w.WriteHeader(200)
 }
 
-func addMenu(w http.ResponseWriter, r *http.Request) {
-  c := appengine.NewContext(r)
-  menu := new(Menu)
-  err := json.NewDecoder(r.Body).Decode(menu)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-  params := mux.Vars(r)
-  date := params["date"]
-  if date != menu.Date {
-    http.Error(w, "Invalid date", http.StatusUnprocessableEntity)
-  }
-  key := datastore.NewKey(c, "Menu", menu.Date, 0, nil)
-  key, err = datastore.Put(c, key, menu)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
+func createMenu(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	// Get date from URL
+	params := mux.Vars(r)
+	date := params["date"]
+
+	// Get items from Body
+	var items []string
+	err := json.NewDecoder(r.Body).Decode(&items)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Save Menu
+	menu := new(Menu)
+	menu.Date = date
+	bytes, err := json.Marshal(items)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	menu.Items = string(bytes)
+	key := datastore.NewKey(c, "Menu", date, 0, nil)
+	key, err = datastore.Put(c, key, menu)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
+func deleteMenu(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	// Get date from URL
+	params := mux.Vars(r)
+	date := params["date"]
+
+	log.Infof(c, "Deleting menu for [%s]", date)
+
+	// Delete Menu
+	key := datastore.NewKey(c, "Menu", date, 0, nil)
+	err := datastore.Delete(c, key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func getMenuRequests(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	// Get date from URL
+	params := mux.Vars(r)
+	date := params["date"]
+
+	// Query MenuRequests for this date
+	q := datastore.NewQuery("MenuRequest").Filter("Date =", date)
+	var requests []MenuRequest
+	_, err := q.GetAll(c, &requests)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Convert to DTOs
+	dtos := make([]MenuRequestDto, len(requests))
+	for i, request := range requests {
+		dto := new(MenuRequestDto)
+		dto.Date = request.Date
+		dto.Homeroom = request.Homeroom
+		dto.Choices = make(map[string]int)
+		err := json.Unmarshal([]byte(request.Choices), &dto.Choices)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		dtos[i] = *dto
+	}
+
+	// Return as JSON
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(dtos)
+	w.WriteHeader(200)
+}
+
+func createMenuRequest(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	// Get keys from URL
+	params := mux.Vars(r)
+	date := params["date"]
+	homeroom := params["homeroom"]
+
+	// Get choices from Body
+	var choices map[string]int
+	err := json.NewDecoder(r.Body).Decode(&choices)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	bytes, err := json.Marshal(choices)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Create MenuRequest
+	request := new(MenuRequest)
+	request.Date = date
+	request.Homeroom = homeroom
+	request.Choices = string(bytes)
+
+	log.Infof(c, "Saving MenuRequest for [%s] and [%s]", date, homeroom)
+
+	// Store MenuRequest
+	id := date + "|" + homeroom
+	key := datastore.NewKey(c, "MenuRequest", id, 0, nil)
+	key, err = datastore.Put(c, key, request)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func deleteMenuRequest(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	// Get keys from URL
+	params := mux.Vars(r)
+	date := params["date"]
+	homeroom := params["homeroom"]
+
+	log.Infof(c, "Deleting MenuRequest for [%s] and [%s]", date, homeroom)
+
+	// Delete MenuRequest
+	id := date + "|" + homeroom
+	key := datastore.NewKey(c, "MenuRequest", id, 0, nil)
+	err := datastore.Delete(c, key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
